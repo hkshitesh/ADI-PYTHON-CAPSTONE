2ab678582c5e1d3504aa6f8832a86ab6964d096c
krbhanushali <43782524+krbhanushali@users.noreply.github.com>
2023-10-31 15:32:07 +0530

    mvc and task2

---
+++ DAY-2/Krishna-Bhanushali/demo-design-patterns/demo-mvc/controller.py
@@ -0,0 +1,11 @@
+class Controller:
+    def __init__(self, model, view):
+        self._model = model
+        self._view = view
+    
+    def set_data(self, data):
+        self._model.set_data(data=data)
+    
+    def update_view(self):
+        data = self._model.get_data()
+        self._view.show_data(data)
