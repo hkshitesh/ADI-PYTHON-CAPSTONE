566f4fef0b26c3156fc1fbf6b10f228ece049613
Sai Kiran Gudla <118167755+SaikiranGudla@users.noreply.github.com>
2023-10-31 18:33:50 +0530

    Task 6 Solution

---
+++ saikiran_gudla/task6_unit_test/string_functions.py
@@ -0,0 +1,13 @@
+def reverse_string(s):
+    return s[::-1]
+def is_palindrome(s):
+    return s == s[::-1]
+def count_characters(s):
+    return len(s)
+def join_strings(s1, s2):
+    return s1 + " " + s2
+
+
+print(reverse_string("s"))
+print(is_palindrome("sai"))
+print(join_strings("sai","kiran"))
