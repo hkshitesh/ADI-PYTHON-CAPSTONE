727705fac4cec9e52ce1cb0dd7b3fd9c73ac0421
selvariyan-adi <139548483+selvariyan-adi@users.noreply.github.com>
2023-10-31 18:28:36 +0530

    Moved functions to new file

--- DAY-2/selvariyan-natarajan/task6_solution.py
+++ DAY-2/selvariyan-natarajan/task6_solution.py
@@ -1,20 +1,5 @@
 import unittest
-
-
-def reverse_string(s):
-    return s[::-1]
-
-
-def is_palindrome(s):
-    return s == s[::-1]
-
-
-def count_characters(s):
-    return len(s)
-
-
-def join_strings(s1, s2):
-    return s1 + "" + s2
+from string_functions import *
 
 
 class TaskTest(unittest.TestCase):
