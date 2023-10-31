ae47f8e4e6aa9bba91dd95a4be7d380221052953
SainandanD <91470819+SainandanD@users.noreply.github.com>
2023-10-31 18:04:21 +0530

    Add files via upload

---
+++ DAY-2/sainandan/task5_package/module1.py
@@ -0,0 +1,9 @@
+"""
+Module 1 to create package
+"""
+
+def hello_module1():
+    """
+    demo function from module 1 to print
+    """
+    print("Hello from Module 1")
---
+++ DAY-2/sainandan/task5_package/module2.py
@@ -0,0 +1,9 @@
+"""
+Module 2 to create package
+"""
+
+def hello_module2():
+    """
+    demo function from module 2 to print
+    """
+    print("Hello from Module 2")
