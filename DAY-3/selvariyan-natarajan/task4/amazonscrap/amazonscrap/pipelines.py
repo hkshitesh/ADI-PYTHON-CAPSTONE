8ae893fa3714008f208f25d896bb80df1e3a5a13
selvariyan-adi <139548483+selvariyan-adi@users.noreply.github.com>
2023-11-02 19:06:22 +0530

    Task 4 Solution

---
+++ DAY-3/selvariyan-natarajan/task4/amazonscrap/amazonscrap/pipelines.py
@@ -0,0 +1,20 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import pymongo
+
+
+class AmazonscrapPipeline:
+    def __init__(self):
+        self.conn = pymongo.MongoClient("mongodb://localhost:27017")
+        db = self.conn["mycollection"]
+        self.collection = db["price_tb"]
+
+    def process_item(self, item, spider):
+        self.collection.insert_one(dict(item))
+        return item
