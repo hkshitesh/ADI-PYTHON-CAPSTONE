5c27ba1c90cb79204eceec2c267ac4347563cf4b
SWAROOP <47825893+SwaroopPK@users.noreply.github.com>
2023-11-02 17:02:48 +0530

    Update Task2 Solution.py

--- DAY-3/Swaroop/Task2 Solution.py
+++ DAY-3/Swaroop/Task2 Solution.py
@@ -6,7 +6,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='Sanjeevini@123',
+    password='************',
     database='university'
 )
 
