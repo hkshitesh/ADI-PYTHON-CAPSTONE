483d3e20682c87972f73de9d328c71ac0be50cc5
Bibhupad <35190794+Bibhupad@users.noreply.github.com>
2023-11-02 17:02:15 +0530

    Add files via upload

--- DAY-3/Bibhupad_day_3/demo-mysql/demoMysql.py
+++ DAY-3/Bibhupad_day_3/demo-mysql/demoMysql.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = 'Precision@1',
+    password = 'Pass@1',
     database = 'mydatabase'
 )
 
