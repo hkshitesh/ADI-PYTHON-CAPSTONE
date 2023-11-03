12b92333a3620ba401fe1f2eac0563b42166cbed
selvariyan-adi <139548483+selvariyan-adi@users.noreply.github.com>
2023-11-03 17:23:49 +0530

    Task 3 Solution

---
+++ DAY-4/selvariyan-natarajan/task3_solution.py
@@ -0,0 +1,78 @@
+class Account:
+    def __init__(self, acc_num, balance):
+        self.account_number = acc_num
+        self.balance = balance
+
+    def display_account_info(self):
+        print(f"Account Number : {self.account_number}")
+        print(f"Account Balance : {self.balance}")
+
+    def deposit(self,amount):
+        pass
+    
+    def withdraw(self, amount):
+        pass
+
+
+class SavingsAccount(Account):
+    def __init__(self, acc_num, balance, int_rate):
+        super().__init__(acc_num, balance)
+        self.interest_rate = int_rate
+    
+    def add_interest(self):
+        self.deposit(self.balance * (self.interest_rate/100))
+
+    def display_account_info(self):
+        super().display_account_info()
+        print(f"Interest Rate : {self.interest_rate}")
+
+    def deposit(self,amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print(f"Can't Withdraw {amount}. Account balance : {self.balance}")
+        else:
+            self.balance -= amount
+
+
+class CheckingAccount(Account):
+    def __init__(self, acc_num, balance, trans_limit):
+        super().__init__(acc_num, balance)
+        self.transaction_limit = trans_limit
+    
+    def process_transaction(self,amount):
+        if self.transaction_limit:
+            self.withdraw(amount)
+            self.transaction_limit -= 1
+        else:
+            print("Exceeded transaction limit")
+    
+    def display_account_info(self):
+        super().display_account_info()
+        print(f"Transaction limit : {self.transaction_limit}")
+
+    def deposit(self,amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print(f"Can't Withdraw {amount}. Account balance : {self.balance}")
+        else:
+            self.balance -= amount
+
+
+if __name__ == "__main__":
+    s = SavingsAccount(12345, 100, 10)
+    c = CheckingAccount(456, 50, 2)
+
+    s.display_account_info()
+    s.add_interest()
+    s.display_account_info()
+
+    c.display_account_info()
+    c.process_transaction(10)
+    c.display_account_info()
+    c.process_transaction(10)
+    c.process_transaction(10)
+    c.display_account_info()
